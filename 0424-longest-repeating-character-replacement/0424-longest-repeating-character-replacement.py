dab694aae161d63236784be66dcc253b8e08d2ba
kevin7548 <jongmin.kim.k@gmail.com>
2026-03-16 16:43:55 +0900

    Time: 107 ms (41.83%), Space: 19.8 MB (12.9%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -6,13 +6,10 @@ class Solution:
 
         for right in range(len(s)):
             counts[s[right]] = counts.get(s[right], 0) + 1
-            length, max_count = sum(counts.values()), max(counts.values())
-            
-            while length - max_count > k:
-                counts[s[left]] = counts.get(s[left]) - 1
+            if right - left + 1 - max(counts.values()) > k:    # 어차피 한 번만
+                counts[s[left]] -= 1
                 left += 1
-                length -= 1
 
-            max_len = max(max_len, length)
+            max_len = max(max_len, right - left + 1)
 
         return max_len
