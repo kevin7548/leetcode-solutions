28710018c60f67ce5f40bd6844f2849a6adc3a56
kevin7548 <jongmin.kim.k@gmail.com>
2026-03-16 16:09:21 +0900

    Time: 11 ms (22.48%), Space: 22.5 MB (78.22%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,18 +2,24 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
+        self.min_stack = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-
+        if self.min_stack and val > self.min_stack[-1]:
+            self.min_stack.append(self.min_stack[-1])
+        else:
+            self.min_stack.append(val)
+        
     def pop(self) -> None:
         self.stack.pop()
+        self.min_stack.pop()
 
     def top(self) -> int:
         return self.stack[-1]
 
-    def getMin(self) -> int:
-        return min(self.stack)
+    def getMin(self) -> int:    # 이게 min 쓰면 O(n)
+        return self.min_stack[-1]
         
 
 
