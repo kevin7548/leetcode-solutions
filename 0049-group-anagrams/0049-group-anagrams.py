2087012f5a8aae69fe4b165420efe9855da33632
kevin7548 <jongmin.kim.k@gmail.com>
2026-04-20 18:11:55 +0900

    Time: 8 ms (92.76%), Space: 22.6 MB (43.79%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,9 +2,9 @@ from collections import defaultdict
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        seen = defaultdict(list)   # {기준: [문자열]}
+        str_dict = defaultdict(list)   # {기준: [문자열]}
         for string in strs:
             key = tuple(sorted(string))
-            seen[key].append(string)
+            str_dict[key].append(string)
 
-        return list(seen.values())
+        return list(str_dict.values())
