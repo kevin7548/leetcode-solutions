094548b462e99c6327fb2f4b7fd1e463f367c01c
kevin7548 <jongmin.kim.k@gmail.com>
2026-04-20 18:21:17 +0900

    Time: 4 ms (67.25%), Space: 22.8 MB (65.39%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,7 +2,5 @@ from collections import Counter
 
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        return [num for num, _ in Counter(nums).most_common(k)]
-
-    # for 루프는 list comprehension으로
-    # Counter, most_common 리스트 정의 안하고 바로 사용
+        count = Counter(nums)
+        return [x for x, _ in count.most_common(k)]
