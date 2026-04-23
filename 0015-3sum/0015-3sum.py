05cea4a7cbe58dd0486bd63604fee26de72243fa
kevin7548 <jongmin.kim.k@gmail.com>
2026-04-23 18:46:31 +0900

    Time: 656 ms (39.56%), Space: 22.4 MB (20.41%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,32 +1,24 @@
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        # O(N^2)까지 가능
-        # 양 끝을 고정하고 Mid 순회가 아니라, left 고정 후 mid, right 좁히기
-        nums.sort()
-        triplet = []
-        
-        # left: i [-4, -1, -1, 0, 1, 2]
-        for i in range(len(nums)-2):
-            if i > 0 and nums[i] == nums[i - 1]:    # nums 중복 skip
-                continue
-            
-            left, right = i+1, len(nums)-1
+        nums.sort() # [-4, -1, -1, 0, 1, 3]
+        result = []
 
+        for i in range(len(nums)):
+            if i > 0 and nums[i] == nums[i - 1]:    # i에 대한 중복
+                continue
+
+            left, right = i + 1, len(nums) - 1
             while left < right:
-                sum_triplets = nums[i] + nums[left] + nums[right]
-                
-                if sum_triplets == 0:
-                    triplet.append([nums[i], nums[left], nums[right]])
+                total = nums[i] + nums[left] + nums[right]
+                if total == 0:
+                    result.append([nums[i], nums[left], nums[right]])
                     while left < right and nums[left] == nums[left + 1]:
                         left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right -= 1
                     left += 1
                     right -= 1
-                elif sum_triplets < 0:
+                elif total < 0:
                     left += 1
-                else:
+                else:   # total > 0
                     right -= 1
-            
-        return triplet
-
+        
+        return result
