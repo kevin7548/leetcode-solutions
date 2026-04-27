c96227c7e5e8027f12d66a75bfe6a8f8223d9c09
kevin7548 <jongmin.kim.k@gmail.com>
2026-04-27 17:57:00 +0900

    Time: 79 ms (76.56%), Space: 19.6 MB (52.91%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,18 +1,27 @@
+from collections import Counter
+
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left = 0
+        counts = {}
         max_len = 0
         max_count = 0
-        counts = {}
+        left = 0
 
         for right in range(len(s)):
+            # 1. 원소 count에 추가
             counts[s[right]] = counts.get(s[right], 0) + 1
-            # 매번 max 계산하지 말고, max_count만 비교
+            # 2. max_count
             max_count = max(max_count, counts[s[right]])
-            if right - left + 1 - max_count > k:    # 어차피 한 번만
+            # 3. left. 가장 길었던 윈도우를 찾으므로 굳이 while X, 윈도우 길이 유지
+            if (right - left + 1) - max_count > k:
                 counts[s[left]] -= 1
                 left += 1
-
+            # 4. max_len
             max_len = max(max_len, right - left + 1)
-
+        
         return max_len
+            
+            
+
+
+            
