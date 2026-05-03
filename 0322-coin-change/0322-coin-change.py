f1415a211c54dabb042b70cbe1ce4d78642a0144
kevin7548 <jongmin.kim.k@gmail.com>
2026-05-03 16:50:28 +0900

    Time: 448 ms (87.98%), Space: 19.6 MB (77.6%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,8 +2,8 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [float('inf')] * (amount + 1)
         dp[0] = 0
-        for i in range(1, amount + 1):  # i: 총합
+        for i in range(1, amount + 1):
             for coin in coins:
-                if coin <= i and dp[i - coin] != float('inf'):  # dp[i - coin] 조건은 보충
+                if coin <= i:   # dp[i - coin] 존재
                     dp[i] = min(dp[i], dp[i - coin] + 1)
         return dp[amount] if dp[amount] != float('inf') else -1
