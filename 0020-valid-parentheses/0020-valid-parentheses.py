15289ea1c7a51f89a77d8b80a66ad147e1b6ad4a
kevin7548 <jongmin.kim.k@gmail.com>
2026-05-13 17:33:40 +0900

    Time: 0 ms (100%), Space: 19.3 MB (27.21%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,11 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        pairs = {')': '(', '}': '{', ']': '['}
-
+        pairs = {')':'(', '}':'{', ']':'['}
         for char in s:
-            if char in pairs: # 닫는 기호
-                last = stack.pop() if stack else '#'
-                if pairs[char] == last:
-                    continue
-                else:
+            if char in pairs:   # 닫는 괄호
+                if not stack or stack.pop() != pairs[char]:
                     return False
-            else: # 여는 기호
+            else:   # 여는 괄호
                 stack.append(char)
-
         return not stack
