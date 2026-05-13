ad558f993c71495decbe78d47fa68592ece51042
kevin7548 <jongmin.kim.k@gmail.com>
2026-05-14 02:26:33 +0900

    Time: 11 ms (25.19%), Space: 23.1 MB (37.88%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.py
+++ 0056-merge-intervals/0056-merge-intervals.py
@@ -2,9 +2,7 @@ class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort(key=lambda x: x[0])
         result = [intervals[0]]
-        #if len(intervals) == 1:
-        #    return result
-        for start, end in intervals[1:]:
+        for start, end in intervals[1:]:    # 리스트 슬라이싱은 인덱싱 에러 X, 그냥 빈 리스트로 루프 통과
             if start <= result[-1][1]:
                 result[-1][1] = max(result[-1][1], end)
             else:
