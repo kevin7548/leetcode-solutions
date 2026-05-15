136e3f048c78028f52ac314c0ad74faf89d86574
kevin7548 <jongmin.kim.k@gmail.com>
2026-05-16 01:17:30 +0900

    Time: 0 ms (100%), Space: 20.7 MB (9.18%) - LeetHub

--- 0704-binary-search/0704-binary-search.py
+++ 0704-binary-search/0704-binary-search.py
@@ -1,16 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
-
+        left, right = 0, len(nums)-1
         while left <= right:
             mid = (left + right) // 2
-            if target < nums[mid]:
-                right = mid - 1
-            elif target > nums[mid]:
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
                 left = mid + 1
             else:
-                return mid
-
-        # target 일일이 찾아서 없는 경우 확인 O(N)이라 비효율적
+                right = mid - 1
         return -1
