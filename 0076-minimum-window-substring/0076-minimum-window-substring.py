97be5daa42b48fd821acb27e65e3c5dcb22874d3
kevin7548 <jongmin.kim.k@gmail.com>
2026-05-19 17:30:55 +0900

    Time: 55 ms (92.25%), Space: 19.8 MB (37.5%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -2,29 +2,36 @@ from collections import Counter, defaultdict
 
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        need = Counter(t)
-        have, need_count = 0, len(need)
-        window = defaultdict(int)
-        result = ""
+        # 1. 목표 문자열 t
+        target = Counter(t)  # 목표 문자들
+        total_count = len(target)    # 만족 문자 수
+        
+        # 2. 슬라이딩 윈도우
+        seen = defaultdict(int)   # 현재 윈도우 문자 {문자: 개수}
+        count = 0
+
         min_len = float('inf')
+        ans_l, ans_r = 0, 0
         left = 0
 
-        for right in range(len(s)):
-            char = s[right]
-            # window, have
-            window[char] += 1
-            if char in need and window[char] == need[char]:
-                have += 1
-        
-            while have == need_count:
-                # result
-                if (right - left + 1) < min_len:
+        # 3. right 확장하며 탐색
+        for right, char in enumerate(s):
+            seen[char] += 1
+            if seen[char] == target[char]: # char 개수 만족시
+                count += 1
+            
+            # 4. 조건 만족 시 left 당기기
+            while count == total_count:
+                if right - left + 1 < min_len:
                     min_len = right - left + 1
-                    result = s[left:right + 1]
-                # left 이동
-                window[s[left]] -= 1
-                if s[left] in need and window[s[left]] < need[s[left]]:
-                    have -= 1
+                    ans_l, ans_r = left, right
+                
+                seen[s[left]] -= 1
+                if seen[s[left]] < target[s[left]]:
+                    count -= 1
+                
                 left += 1
 
-        return result
+        return s[ans_l:ans_r + 1] if min_len != float('inf') else ""
+
+# min_len을 통해 길이만 비교, 인덱스 저장 후 최종적으로 슬라이싱 마지막에.
