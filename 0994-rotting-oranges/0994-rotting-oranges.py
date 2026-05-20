dc007a74fba374de1d976fefeb222c649ec249e3
kevin7548 <jongmin.kim.k@gmail.com>
2026-05-20 18:20:23 +0900

    Time: 6 ms (19.89%), Space: 19.4 MB (12.76%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        R, C = len(grid), len(grid[0])
+        max_t, fresh_count = 0, 0
+
+        # 1. rotten orange 찾기
+        q = deque([])
+        for r in range(R):
+            for c in range(C):
+                if grid[r][c] == 2:
+                    q.append((r, c, 0))
+                if grid[r][c] == 1:
+                    fresh_count += 1
+        
+        # 2. bfs
+        while q:
+            r, c, t = q.popleft()
+            max_t = max(t, max_t)
+            for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+                nr, nc = r+dr, c+dc
+                if 0<=nr<R and 0<=nc<C and grid[nr][nc] == 1:
+                    q.append((nr, nc, t+1))
+                    grid[nr][nc] = 2
+                    fresh_count -= 1
+
+        return max_t if fresh_count == 0 else -1
+
+        
