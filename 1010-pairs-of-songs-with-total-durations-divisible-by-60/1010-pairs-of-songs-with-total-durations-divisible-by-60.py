08bc6b9102e339dbb8d243a3a45a4468d43f9c55
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-01 17:25:05 +0900

    Time: 32 ms (27.15%), Space: 23.3 MB (12.79%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -3,10 +3,10 @@ from collections import defaultdict
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
         answer = 0
-        duration = defaultdict(list)   # {나머지: [인덱스]}
+        duration = defaultdict(int)   # {나머지: 개수} 0<=t<60 인덱스에 대한 리스트 만드는 것 비효율적
         for i, t in enumerate(time):
-            complement = (540 - t) % 60
+            complement = (60 - t % 60) % 60
             if complement in duration:
-                answer += len(duration[complement])
-            duration[t % 60].append(i)
+                answer += duration[complement]
+            duration[t % 60] += 1
         return answer
