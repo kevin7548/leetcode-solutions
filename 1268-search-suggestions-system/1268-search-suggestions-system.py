8ede8be797145e90f69d3afc11c63572c166233f
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-02 17:52:06 +0900

    Time: 1 ms (97.28%), Space: 22.3 MB (64.32%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.py
+++ 1268-search-suggestions-system/1268-search-suggestions-system.py
@@ -1,15 +1,19 @@
+import bisect
+
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
         result = []
-        for l in range(1, len(searchWord)+1):
-            prefix = searchWord[:l]   # 길이 l
-            ls = []
-            for p in products:
-                if p[:l] == prefix:
-                    ls.append(p)
-                if len(ls) == 3:
+        prefix = ""
+        for char in searchWord:
+            prefix += char  # prefix 일일이 인덱싱 X
+            suggestions = []
+            left = bisect.bisect_left(products, prefix) # bisect 활용 인덱싱
+            for i in range(left, min(left + 3, len(products))):
+                if products[i].startswith(prefix):
+                    suggestions.append(products[i])
+                else:
                     break
-            result.append(ls)
+            result.append(suggestions)
         return result
 
