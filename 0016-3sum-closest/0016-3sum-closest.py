90fbdf3e3a853141c5d0b96e7af0f997305c4f46
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-03 17:05:25 +0900

    Time: 521 ms (5.77%), Space: 19.4 MB (46.09%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -3,15 +3,23 @@ class Solution:
         nums.sort()
         result = float('inf')
         for i in range(len(nums)-2):
+            if i > 0 and nums[i] == nums[i - 1]:    # 중복인 경우 스킵
+                continue
             left, right = i + 1, len(nums) - 1
             while left < right:
                 sum_3 = nums[i] + nums[left] + nums[right]
+                if sum_3 == target: # 일치하는 경우 조기 종료
+                    return sum_3
                 if abs(target - sum_3) < abs(target - result):
                     result = sum_3
                 if target < sum_3:
                     right -= 1
+                    while left < right and nums[right] == nums[right + 1]:  # 중복
+                        right -= 1
                 else:
                     left += 1
+                    while left < right and nums[left] == nums[left - 1]:    # 중복
+                        left += 1
         return result
                 
         -4,-1,2
