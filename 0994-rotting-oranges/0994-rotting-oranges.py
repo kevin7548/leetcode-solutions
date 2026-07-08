8cc132aa9ebd1bda48355b728f807aedad2ea51c
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-08 18:22:01 +0900

    Time: 3 ms (75.49%), Space: 19.4 MB (40.9%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -2,29 +2,28 @@ from collections import deque
 
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        R, C = len(grid), len(grid[0])
-        max_t, fresh_count = 0, 0
+        m, n = len(grid), len(grid[0])
+        queue = deque([])
+        fresh = 0   # fresh도 같이 추적해야!!
 
-        # 1. rotten orange 찾기 & fresh orange 세기
-        q = deque([])
-        for r in range(R):
-            for c in range(C):
-                if grid[r][c] == 2:
-                    q.append((r, c, 0))
-                elif grid[r][c] == 1:
-                    fresh_count += 1
+        # rotten, fresh 파악
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 2:
+                    queue.append((i,j))
+                elif grid[i][j] == 1:
+                    fresh += 1
         
-        # 2. multi-source BFS
-        while q:
-            r, c, t = q.popleft()
-            max_t = max(t, max_t)
-            for dr, dc in [(-1,0),(1,0),(0,-1),(0,1)]:
-                nr, nc = r+dr, c+dc
-                if 0<=nr<R and 0<=nc<C and grid[nr][nc] == 1:
-                    q.append((nr, nc, t+1))
-                    grid[nr][nc] = 2
-                    fresh_count -= 1
+        minutes = 0
+        while queue and fresh:
+            for _ in range(len(queue)):
+                r, c = queue.popleft()
+                for dr, dc in [(-1,0),(1,0),(0,1),(0,-1)]:
+                    nr, nc = r+dr, c+dc
+                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
+                        grid[nr][nc] = 2
+                        queue.append((nr, nc))
+                        fresh -= 1
+            minutes += 1
 
-        return max_t if fresh_count == 0 else -1
-
-        
+        return -1 if fresh else minutes
