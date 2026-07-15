c1ff70fa65bff2d56b9db25d80b54974ab84baf8
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-15 17:38:09 +0900

    Time: 1205 ms (59.65%), Space: 33.6 MB (58.95%) - LeetHub

--- 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.py
+++ 1898-maximum-number-of-removable-characters/1898-maximum-number-of-removable-characters.py
@@ -1,20 +1,20 @@
 class Solution:
     def maximumRemovals(self, s: str, p: str, removable: List[int]) -> int:
-        def sub_check(k):
-            j = 0   # p-index
-            removed = set(removable[:k])
+        def subseq(K):
+            removed = set(removable[:K])
+            j = 0   # 다음에 매칭할 p의 인덱스
             for i in range(len(s)):
                 if i in removed:
                     continue
-                if j < len(p) and s[i] == p[j]: # j의 범위!!
+                if j < len(p) and s[i] == p[j]:
                     j += 1
-            return j == len(p)
+            return j == len(p) # 그래야 p 모두 매칭완료.
         
-        left, right = 0, len(removable) # left(yes), right(no)
-        while left < right:
-            mid = (left + right + 1) // 2
-            if sub_check(mid):
-                left = mid
+        lo, hi = 0, len(removable)
+        while lo < hi:  # lo(yes), hi(no)
+            mid = (lo + hi + 1) // 2
+            if subseq(mid):
+                lo = mid
             else:
-                right = mid - 1
-        return left
+                hi = mid - 1
+        return lo
