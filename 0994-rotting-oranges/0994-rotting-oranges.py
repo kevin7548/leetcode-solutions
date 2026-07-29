ec4c77a21d8f0e824e8299ea038a6332ae4753d6
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-29 16:12:25 +0900

    Time: 3 ms (75.09%), Space: 19.5 MB (12.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -3,27 +3,33 @@ from collections import deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        queue = deque([])
-        fresh = 0   # fresh도 같이 추적해야!!
+        queue = deque()
+        cur_fr, total_fr = 0, 0
+        minute = 0
 
-        # rotten, fresh 파악
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
-                    queue.append((i,j))
+                    queue.append((i, j))
                 elif grid[i][j] == 1:
-                    fresh += 1
+                    total_fr += 1
         
-        minutes = 0
-        while queue and fresh:
-            for _ in range(len(queue)):
-                r, c = queue.popleft()
-                for dr, dc in [(-1,0),(1,0),(0,1),(0,-1)]:
-                    nr, nc = r+dr, c+dc
-                    if 0 <= nr < m and 0 <= nc < n and grid[nr][nc] == 1:
-                        grid[nr][nc] = 2
-                        queue.append((nr, nc))
-                        fresh -= 1
-            minutes += 1
+        if total_fr == 0:
+            return 0
 
-        return -1 if fresh else minutes
+        while queue:
+            minute += 1
+            for _ in range(len(queue)):
+                x, y = queue.popleft()
+                for dx, dy in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nx, ny = x+dx, y+dy
+                    if 0<= nx < m and 0 <= ny < n and grid[nx][ny] == 1:
+                        grid[nx][ny] = 2
+                        cur_fr += 1
+                        queue.append((nx, ny))
+            
+            if cur_fr == total_fr:
+                break
+        
+        return -1 if cur_fr != total_fr else minute
+            
