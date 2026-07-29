2b9892e30bce9f628ea2a539b0795cb5bb4b9bbe
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-29 15:23:13 +0900

    Time: 233 ms (84.67%), Space: 21.7 MB (47.65%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,19 +3,17 @@ class Solution:
         count = 0
         m, n = len(grid), len(grid[0])
 
-        def bfs(x, y):
-            if not grid[x][y]:
-                return
+        def dfs(x, y):
             for dx, dy in [(0,1),(0,-1),(1,0),(-1,0)]:
                 nx, ny = x+dx, y+dy
                 if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == '1':
                     grid[nx][ny] = '0'
-                    bfs(nx, ny)
+                    dfs(nx, ny)
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
                     count += 1
-                    bfs(i, j)
+                    dfs(i, j)
 
         return count
