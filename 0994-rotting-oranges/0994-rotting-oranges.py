faddaa40cf50b457a0e38e0b2f49c3608a7a9a16
kevin7548 <jongmin.kim.k@gmail.com>
2026-07-29 16:21:52 +0900

    Time: 3 ms (75.09%), Space: 19.2 MB (78.29%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -13,11 +13,8 @@ class Solution:
                     queue.append((i, j))
                 elif grid[i][j] == 1:
                     total_fr += 1
-        
-        if total_fr == 0:
-            return 0
 
-        while queue:
+        while queue and cur_fr < total_fr:  # while문 조건 여러 개
             minute += 1
             for _ in range(len(queue)):
                 x, y = queue.popleft()
@@ -27,9 +24,6 @@ class Solution:
                         grid[nx][ny] = 2
                         cur_fr += 1
                         queue.append((nx, ny))
-            
-            if cur_fr == total_fr:
-                break
         
-        return -1 if cur_fr != total_fr else minute
+        return minute if cur_fr == total_fr else -1
             
