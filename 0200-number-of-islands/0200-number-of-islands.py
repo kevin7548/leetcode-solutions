b4acce5ea8bffd2ffc4d2fe9dbfe7a88a3c9c446
kevin7548 <jongmin.kim.k@gmail.com>
2026-08-01 19:42:05 +0900

    Time: 241 ms (64.13%), Space: 21.5 MB (91.34%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,19 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
         m, n = len(grid), len(grid[0])
+        count = 0
 
-        def dfs(x, y):
+        def dfs(x, y):  # 인접한 곳 탐색, 1을 0으로 변환
             for dx, dy in [(0,1),(0,-1),(1,0),(-1,0)]:
                 nx, ny = x+dx, y+dy
                 if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == '1':
                     grid[nx][ny] = '0'
                     dfs(nx, ny)
-        
+
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == '1':
                     count += 1
                     dfs(i, j)
-
+        
         return count
