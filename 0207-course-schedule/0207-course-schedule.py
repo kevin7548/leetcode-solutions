4ce8ae0d71fdb840cb53f2e4b0549f29395dad2e
kevin7548 <jongmin.kim.k@gmail.com>
2026-08-04 16:47:57 +0900

    Time: 2 ms (88.99%), Space: 20.3 MB (92.78%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,27 +1,26 @@
-from collections import deque, defaultdict
+from collections import defaultdict, deque
 
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        indeg = [0] * numCourses    # 안 들은 선수과목 수
-        graph = defaultdict(list)   # 선수과목: [과목]
+        indeg = [0] * numCourses    # 들어야하는 선수과목 수
+        graph = defaultdict(list)   # {선수과목: [과목]}
         queue = deque()
-        taken = 0
-
-        for a, b in prerequisites:  # a: 과목 b: 선수과목
+        
+        # graph, indeg
+        for a, b in prerequisites:  # a:과목, b: 선수과목
             graph[b].append(a)
             indeg[a] += 1
 
-        for idx in range(len(indeg)):
+        # queue
+        for idx in range(numCourses):
             if indeg[idx] == 0:
                 queue.append(idx)
-                taken += 1
-        
+
         while queue:
-            course = queue.popleft()
-            for c in graph[course]:
+            i = queue.popleft()
+            for c in graph[i]:
                 indeg[c] -= 1
                 if indeg[c] == 0:
-                    queue.append(c) # 선수과목 다 들은 것만 큐에
-                    taken += 1
+                    queue.append(c)
 
-        return taken == numCourses
+        return indeg == [0] * numCourses
