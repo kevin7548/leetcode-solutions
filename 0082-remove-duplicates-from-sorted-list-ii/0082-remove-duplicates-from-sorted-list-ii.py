cd17617927bc089f7090a2581565f06ec2ae90dd
kevin7548 <jongmin.kim.k@gmail.com>
2026-08-15 17:17:05 +0900

    Time: 0 ms (100%), Space: 19.2 MB (73.03%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -6,17 +6,16 @@
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(-101, head)
-        prev = dummy    # 현재까지 확정
-        curr = head    # 검사하는 노드
+        prev, curr = dummy, head
 
         while curr:
             if curr.next and curr.val == curr.next.val:
-                val = curr.val 
-                while curr and val == curr.val:
+                value = curr.val
+                while curr and curr.val == value:
                     curr = curr.next
                 prev.next = curr
             else:
                 prev = curr
-                curr = curr.next
-
+                curr = curr.next    # curr가 마지막 노드면 none으로 이동
+        
         return dummy.next
