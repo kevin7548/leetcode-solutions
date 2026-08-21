07130f21d72ed9ee98745125f5dd1099f5666342
kevin7548 <jongmin.kim.k@gmail.com>
2026-08-21 17:02:59 +0900

    Time: 198 ms (34.08%), Space: 19.9 MB (30.9%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,11 +1,17 @@
+from collections import defaultdict
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        seen = {} # {값: 인덱스}
-        left = 0
-        max_len = 0
-        for right, char in enumerate(s):
-            if char in seen and seen[char] >= left:
-                left = seen[char] + 1
-            seen[char] = right
+        max_len, left = 0, 0
+
+        for right in range(len(s)):
+            # 현재 substring에 있는지 확인
+            ch, S = s[right], s[left:right]
+            if ch in S:
+                # ch=a right=4
+                left = left + S.index(ch) + 1 # 2
             max_len = max(max_len, right - left + 1)
+
         return max_len
+
+        # 해시 안 쓴 이유: left 당길 때 삭제하기 너무 번거롭다
