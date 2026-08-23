f724ec060f120cab542a69bfc99a7d5e26787568
kevin7548 <jongmin.kim.k@gmail.com>
2026-08-23 21:28:40 +0900

    Time: 153 ms (20.81%), Space: 19.8 MB (15.69%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -3,14 +3,13 @@ from collections import Counter, defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         left = 0
-        max_len, max_feq = 0, 0
+        max_len = 0
         count = defaultdict(int)  # {문자: 개수}
 
         for right in range(len(s)):
             count[s[right]] += 1
-            max_feq = max(max_feq, count[s[right]])
             # left 갱신
-            if (right-left+1) - max_feq > k:
+            while (right-left+1) - max(count.values()) > k:
                 count[s[left]] -= 1
                 left += 1
             # length 갱신
