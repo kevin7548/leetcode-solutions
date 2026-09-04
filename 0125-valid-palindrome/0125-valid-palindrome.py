0a1fcd5800af9f9c73f639b5fb8398a35f264c95
kevin7548 <jongmin.kim.k@gmail.com>
2026-09-04 17:43:08 +0900

    Time: 4 ms (90.59%), Space: 23.3 MB (12.08%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,12 +5,12 @@ class Solution:
         # for문으로 .isalnum() 확인 후 추가
         # return s == s[::-1]
 
-        s = s.lower()
-        string = ""
-        for char in s:
-            if char.isalnum():
-                string += char
-            else:
-                continue
+        s = [c.lower() for c in s if c.isalnum()]
+        left, right = 0, len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                return False
+            left += 1
+            right -= 1
 
-        return string == string[::-1]
+        return True
